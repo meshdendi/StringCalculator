87ff6e15a32b158ca687b71fb39c97f2efa8da16
abed <asoubra@novelis.io>
2022-05-23 19:09:01 +0200

    add tests to handle null and empty argments

--- src/io/kata/test/StringCalculatorTest.java
+++ src/io/kata/test/StringCalculatorTest.java
@@ -2,7 +2,7 @@ package io.kata.test;
 
 import io.kata.exception.InvalidInputException;
 import io.kata.exception.NegativeNumbersException;
-import io.kata.main.Main;
+import io.kata.helper.StringUtil;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,30 +13,42 @@ public class StringCalculatorTest {
     @Test
     public void testStringCalculatorAddDefaultDelimiter() {
         String arg =";\n1;2;3;6";
-        assertEquals(Main.add(arg), 12);
+        assertEquals(StringUtil.add(arg), 12);
     }
 
     @Test
     public void testStringCalculatorAddPassedDelimiter() {
         String arg ="1;2;3;6";
-        assertEquals(Main.add(arg), 12);
+        assertEquals(StringUtil.add(arg), 12);
     }
 
     @Test
     public void testStringCalculatorHandleNegativeNumbers() {
         String arg ="1;2;3;-6";
-        assertThrows(NegativeNumbersException.class, () -> Main.add(arg));
+        assertThrows(NegativeNumbersException.class, () -> StringUtil.add(arg));
     }
 
     @Test
     public void testStringCalculatorHandleNoNumbersAfterDelimiter() {
         String arg ="1;2;3;\n";
-        assertEquals(Main.add(arg), 0);
+        assertEquals(StringUtil.add(arg), 0);
     }
 
     @Test
     public void testStringCalculatorHandleInvalidInput() {
         String arg ="1;2;3;d";
-        assertThrows(InvalidInputException.class, ()-> Main.add(arg));
+        assertThrows(InvalidInputException.class, ()-> StringUtil.add(arg));
+    }
+
+    @Test
+    public void testStringCalculatorHandleNullInput() {
+        String arg = null;
+        assertEquals(StringUtil.add(arg), 0);
+    }
+
+    @Test
+    public void testStringCalculatorHandleEmptyInput() {
+        String arg = "";
+        assertEquals(StringUtil.add(arg), 0);
     }
 }
