4ca429b37ecb8e416b14f31f4835471de7304d5d
Abed Al Rahim Soubra <soubra.abed@gmail.com>
2022-05-30 21:02:42 +0200

    Rename test class to StringUtilTest in order to follow naming conventions

--- src/io/kata/test/StringUtilTest.java
+++ src/io/kata/test/StringUtilTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class StringCalculatorTest {
+public class StringUtilTest {
 
     @Test
     public void testStringCalculatorAddDefaultDelimiter() {
