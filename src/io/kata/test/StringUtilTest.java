8618a9b91b64e34a6f435f39b2298229929dcf1e
Abed Al Rahim Soubra <soubra.abed@gmail.com>
2022-06-17 14:08:01 +0200

    add teste for negative nbrs exception msg & test for step 1

--- src/io/kata/test/StringUtilTest.java
+++ src/io/kata/test/StringUtilTest.java
@@ -11,6 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class StringUtilTest {
 
     @Test
+    public void testStringCalculatorWithTwoNumbersAsInputAndDefaultDelimiter() {
+        String arg ="5,9";
+        assertEquals(StringUtil.add(arg), 14);
+    }
+
+    @Test
     public void testStringCalculatorAddDefaultDelimiter() {
         String arg =";\n1;2;3;6";
         assertEquals(StringUtil.add(arg), 12);
@@ -26,6 +32,16 @@ public class StringUtilTest {
     public void testStringCalculatorHandleNegativeNumbers() {
         String arg ="1,2,3,-6";
         assertThrows(NegativeNumbersException.class, () -> StringUtil.add(arg));
+    }
+
+    @Test
+    public void testStringCalculatorHandleNegativeNumbersShowNegativeNumbersInErrorMessage() {
+        String arg ="1,2,3,-6,-7";
+        try {
+            StringUtil.add(arg);
+        } catch (NegativeNumbersException ex) {
+            assertEquals(ex.getMessage(), "Negatives not allowed -6, -7");
+        }
     }
 
     @Test
